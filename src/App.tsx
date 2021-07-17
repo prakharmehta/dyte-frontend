02a6f336c6966634b8f4248192afeebea8b9487a
Prakhar Mehta <prakh.mehta@gmail.com>
2021-07-17 21:25:16 +0530

    Added comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
 
+//Import required components
 import FileExplorer from "./components/FileExplorer/FileExplorer";
 import CodeEditor from "./components/CodeEditor/CodeEditor";
 import LiveView from "./components/LiveView/LiveView";
 
+//CSS
 import "./App.css";
 
 const App = () => {
+
+  //Object specifying initital state of the file editors
   const fileStatus = {
     html: false,
     css: false,
     js: false,
   };
 
+  //Array containing meta-info on files
   const files = [
     {
       fileName: "index.html",
@@ -37,11 +42,15 @@ const App = () => {
     },
   ];
 
+  //State hook used to update the html, css and js code dynamically
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+
+  //Statehook used to update the code to be rendered
   const [srcDoc, setSrcDoc] = useState("");
 
+  //Effect hook used to add a delay between the render and the code typed
   useEffect(() => {
     const timeout = setTimeout(() => {
       setSrcDoc(`
@@ -56,8 +65,10 @@ const App = () => {
     return () => clearTimeout(timeout);
   }, [html, css, js]);
 
+  //State hook used to update the status of the file editors
   const [status, setStatus] = useState({ ...fileStatus });
 
+  //Function that handles the open/close of file editors
   const handleVisible = (type: string) => {
     if (type === "html") {
       setStatus({
@@ -83,31 +94,33 @@ const App = () => {
   return (
     <div className="App">
       <div className="App__container">
+        {/* File Explorer Component */}
         <FileExplorer files={files} func={handleVisible} codeToPaste={srcDoc} />
+        {/* HTML Editor */}
         <CodeEditor
           editorTitle={files[0].fileName}
           language={files[0].language}
           value={html}
           onChange={setHtml}
           status={status.html}
-          isVisible={handleVisible}
         />
+        {/* CSS Editor */}
         <CodeEditor
           editorTitle={files[1].fileName}
           language={files[1].language}
           value={css}
           onChange={setCss}
           status={status.css}
-          isVisible={handleVisible}
         />
+        {/* JS Editor */}
         <CodeEditor
           editorTitle={files[2].fileName}
           language={files[2].language}
           value={js}
           onChange={setJs}
           status={status.js}
-          isVisible={handleVisible}
         />
+        {/* Live View Renderer */}
         <LiveView srcDoc={srcDoc} />
       </div>
     </div>
